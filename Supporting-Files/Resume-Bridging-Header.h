93f3c1c47f63a144343eaa8c881289893111bb9d
Kevin Caughman <kevincaughman12@gmail.com>
2015-07-07 22:25:56 -0400

    Create Resume-Bridging-Header.h

---
+++ Supporting-Files/Resume-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+/* This file can be created by adding a new file as an iOS Objectice-c file. When prompt to add a bridgin header file
+click create. The objective-c file can be deleted and you will be left with this file. */
+
+#import <Parse/Parse.h>
+#import <Bolts/Bolts.h>
+#import <ParseUI/ParseUIConstants.h>
