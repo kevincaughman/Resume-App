a7432ebc004e578d75ba6cec99876b8ce81f801e
Kevin Caughman <kevincaughman12@gmail.com>
2015-08-21 14:57:41 -0400

    Create Extensions.swift

---
+++ Models/Extensions.swift
@@ -0,0 +1,95 @@
+//
+//  Extensions.swift
+//  Resume
+//
+//  Created by Kevin Caughman on 7/30/15.
+//  Copyright © 2015 Kevin Caughman. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var length: Int {
+        get {
+            return self.characters.count
+        }
+    }
+    
+    func substring(startIndex: Int, length: Int) -> String {
+        let start = advance(self.startIndex, startIndex)
+        let returnString = self.substringToIndex(advance(start, length))
+        print(returnString)
+        return returnString
+    }
+    
+    func contains(s: String) -> Bool {
+        return (self.rangeOfString(s) != nil) ? true : false
+    }
+    
+    func Slice(start: Int, var end: Int) -> String {
+        // Keep this for negative end support
+        if end < 0 {
+            end = self.characters.count + end
+        }
+        let len = end - start; // Calculate length
+        return substring(start, length: len) // Return Substring of length
+    }
+    
+    
+    func RFind(substring: String, start: Int, var end: Int) -> Int {
+        
+        let len = end - start
+        
+        if (end < 0) {
+            end = self.length + end
+            return self.indexOf(substring, startIndex: start)
+        
+        } else if (len == 0) {
+            return self.lastIndexOf(substring)
+        }
+        return self.indexOf(substring, startIndex: start)
+    }
+    
+    func indexOf(target: String) -> Int {
+        let range = self.rangeOfString(target)
+        if let range = range {
+            return distance(self.startIndex, range.startIndex)
+        } else {
+            return -1
+        }
+    }
+    
+    func indexOf(target: String, startIndex: Int) -> Int {
+        let startRange = advance(self.startIndex, startIndex)
+        let range = self.rangeOfString(target, options: NSStringCompareOptions.LiteralSearch, range: Range<String.Index>(start: startRange, end: self.endIndex))
+        if let range = range {
+            return distance(self.startIndex, range.startIndex)
+        } else {
+            return -1
+        }
+    }
+    
+    func lastIndexOf(target: String) -> Int {
+        var index = -1
+        var stepIndex = self.indexOf(target)
+        
+        while stepIndex > -1 {
+            index = stepIndex
+            if stepIndex + target.length < self.length {
+                stepIndex = indexOf(target, startIndex: stepIndex + target.length)
+            } else {
+                stepIndex = -1
+            }
+        }
+        return index
+    }
+}
+
+extension Array {
+    func randomItem() -> Element {
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self[index]
+    }
+}
+
